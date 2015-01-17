d01c9375e3228b397aed87341392db88318a89b9
edqwerty1 <ed.blacklee@gmail.com>
2015-01-17 15:05:43 +0000

    Added SM and EF

--- WebAPIDnD/Areas/HelpPage/Controllers/HelpController.cs
+++ WebAPIDnD/Areas/HelpPage/Controllers/HelpController.cs
@@ -18,7 +18,7 @@ namespace WebAPIDnD.Areas.HelpPage.Controllers
         {
         }
 
-        public HelpController(HttpConfiguration config)
+        private HelpController(HttpConfiguration config)
         {
             Configuration = config;
         }
---
+++ WebAPIDnD/Domain/Entities/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDnD.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public int Strength { get; set; }
+
+        public int Dexterity { get; set; }
+
+        public int Constitution { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public int Wisdom { get; set; }
+
+        public int Charisma { get; set; }
+    }
+}
